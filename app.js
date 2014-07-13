51f7d9a1b90294516691bfd3d42a4bc7587b354b
Justin Donato <justin.donato@gmail.com>
2014-07-13 17:05:48 -0400

    get basic flow going

--- app.js
+++ app.js
@@ -12,7 +12,71 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
+Users = {
+
+    activeUser: null,
+    usersNeedingContent: [],
+
+    nameExists: function() {
+        return false;
+    },
+
+    create: function(name, socket) {
+        var user = {
+            socket: socket,
+            data: {
+                name: name,
+                isActive: false
+            }
+        };
+        this.usersNeedingContent.push(user);
+        return user;
+    },
+
+    getActive: function() {
+        return this.activeUser;
+    },
+
+    needsContent: function() {
+        return this.usersNeedingContent;
+    }
+}
+
 io.sockets.on('connection', function (socket) {
-    console.log('connected');
+
+    socket.on('user:new', function(desiredName) {
+
+        if (Users.nameExists(desiredName)) {
+            socket.emit('user:name-exists', desiredName);
+        } else {
+            var user = Users.create(desiredName, socket);
+            var activeUser = Users.getActive();
+
+            if (activeUser) {
+                activeUser.socket.emit('editor:get-contents');
+            } else {
+                Users.activeUser = user;
+                user.data.isActive = true;
+            }
+
+            user.socket.emit('user:created', user.data);
+        }
+    });
+
+    socket.on('editor:active-contents', function(content) {
+
+        var users = Users.needsContent();
+
+        for (var i = 0; i < users.length; i++) {
+            users[i].socket.emit('editor:active-contents', content);
+        }
+
+        Users.needingContent = [];
+
+    });
+
+    socket.on('editor:changed', function(args) {
+        socket.broadcast.emit('editor:changed', args);
+    });
 
 });
